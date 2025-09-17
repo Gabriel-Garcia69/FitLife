ba88dac36e5ebfd35ed6b420c4669c8918455a48
DarkShadowXx64 <gabriel.garcia@aasoftware.mx>
2025-09-16 19:03:14 -0500

    Tailwind css installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-center text-black text-9xl">HOLA MUNDO</p>
+      </div>
+    </>
+  )
+}
+
+export default App
